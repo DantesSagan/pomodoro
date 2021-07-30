5f40035ed25c8c253e32bebf004e1111ba5735b1
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-30 14:49:07 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App({ initialState }) {
+  const [counter, setCounter] = React.useState(5);
+  const [session, setSession] = React.useState(25);
+  const [timer, setTimer] = React.useState(initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div
+        className='container algin-items-center justify-content-center '
+        style={{
+          marginTop: '150px',
+          padding: '100px',
+          display: 'block',
+        }}
+      >
+        <h1 className='align-items-start'>25 + 5 Clock</h1>
+        <div className='align-items-center'>
+          <div className='col'>Break Length</div>
+          <button
+            className='col'
+            onClick={() => setCounter((prev) => prev - 1)}
+          >
+            -
+          </button>
+          <div className='col' style={{ fontSize: '1.5rem' }}>
+            {counter}
+          </div>
+          <button
+            className='col'
+            onClick={() => setCounter((prev) => prev + 1)}
+          >
+            +
+          </button>
+        </div>
+
+        <div className='align-items-center'>
+          <div className='col'>Session Length</div>
+          <button
+            className='col'
+            onClick={() => setSession((prev) => prev - 1)}
+          >
+            -
+          </button>
+          <div className='col' style={{ fontSize: '1.5rem' }}>
+            {session}
+          </div>
+          <button
+            className='col'
+            onClick={() => setSession((prev) => prev + 1)}
+          >
+            +
+          </button>
+        </div>
+
+        <div
+          className='align-items-end'
+          style={{
+            fontSize: '2rem',
+            margin: '15px',
+            padding: '15px',
+            backgroundColor: 'wheat',
+            border: '1px solid black',
+            borderRadius: '30px',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {' '}
+          <button className='col' onClick={() => setSession(() => 25)}>
+            +
+          </button>
+          <h2 className='col'>Session:</h2>
+          {session}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
